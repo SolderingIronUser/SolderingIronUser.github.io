0e89d5c8ea41582a64359a33373707ecc6dbd09a
SolderingIronUser <85685236+SolderingIronUser@users.noreply.github.com>
2021-06-10 18:58:30 +0300

    Add files via upload

--- post_message/mail.php
+++ post_message/mail.php
@@ -1,8 +1,10 @@
 <?php 
 
 use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
 require_once('PHPMailerAutoload.php');
+$mail = new PHPMailer;
 $mail->CharSet = 'utf-8';
 
 $name = $_POST['user_name'];
